bed8b83590806607de0814a72aafe67a6f49e109
intern <intern@intern-2.local>
2016-04-06 19:35:19 +0900

    スクリプト名修正

--- OriginalGameKayac/Assets/Script/Destroyer.cs
+++ OriginalGameKayac/Assets/Script/Destroyer.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class Destroy : MonoBehaviour {
+public class Destroyer : MonoBehaviour {
 	
 	[SerializeField] 
 	private AudioClip[] voiceClip;
