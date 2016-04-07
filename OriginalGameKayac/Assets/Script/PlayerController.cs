cf1543b9874a9d9af753f1adef1ce15d5324b8e4
intern <intern@intern-2.local>
2016-04-07 16:17:27 +0900

    LightComponent参照位置とRendererの変数名修正

--- OriginalGameKayac/Assets/Script/PlayerController.cs
+++ OriginalGameKayac/Assets/Script/PlayerController.cs
@@ -6,10 +6,11 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	CharacterController controller;
-	GameObject player, frontLight, backLight;
+	GameObject player;
+	Light frontLight, backLight;
 	Vector3 move = Vector3.zero;
 	Vector3 diff;
-	Renderer mat;
+	Renderer renderer;
 	readonly Vector3 Lane1 = new Vector3 (0, 0, -2); //手前のレーン
 	readonly Vector3 Lane2 = new Vector3 (0, 0, 2);  //奥のレーン
 	int jumpCount = 0;     //ジャンプ回数をカウント
@@ -18,14 +19,14 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField] float jumpPower = 8.0f; //ジャンプの大きさ
 	[SerializeField] float gravity = 20.0f;  //重力
 	[SerializeField] int MaxJumpCount;       //ジャンプ回数上限
-	[SerializeField] Material front, back; //front用とback用のマテリアル
+	[SerializeField] Material frontMaterial, backMaterial; //front用とback用のマテリアル
 
 	void Awake () {
 		controller = GetComponent<CharacterController> ();
-		mat = GetComponent<Renderer> ();
+		renderer = GetComponent<Renderer> ();
 		player = GameObject.Find ("PlayerSphere");
-		frontLight = transform.Find ("FrontLight").gameObject;
-		backLight = transform.Find ("BackLight").gameObject;
+		frontLight = transform.Find ("FrontLight").GetComponent<Light> ();
+		backLight = transform.Find ("BackLight").GetComponent<Light> ();
 
 
 	}
@@ -34,9 +35,7 @@ public class PlayerController : MonoBehaviour {
 		
 		//プレイヤーが死んでいたらreturn
 		if (player.activeSelf == null) return;
-
-		float y = move.y;
-
+        float y = move.y;
 		move = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, 0.0f);
 		move *= speed;
 		move.y += y;
@@ -44,16 +43,15 @@ public class PlayerController : MonoBehaviour {
 		//方向キー↑と↓の入力によってレーン移動とマテリアル変更
 		if (Input.GetKeyDown ("up")) {
 			ChangeLane ();
-			mat.material = back; 
-			frontLight.GetComponent<Light> ().enabled = false;
-			backLight.GetComponent<Light> ().enabled = true;
+			renderer.material = backMaterial; 
+			frontLight.enabled = false;
+			backLight.enabled = true;
 
-		}
-		else if(Input.GetKeyDown ("down")){
+		}else if(Input.GetKeyDown ("down")){
 			ChangeLane ();
-			mat.material = front; 
-			backLight.GetComponent<Light> ().enabled = false;
-			frontLight.GetComponent<Light> ().enabled = true;
+			renderer.material = frontMaterial; 
+			backLight.enabled = false;
+			frontLight.enabled = true;
 		}
 
 		//地面に接地した場合重力とジャンプ回数リセット
